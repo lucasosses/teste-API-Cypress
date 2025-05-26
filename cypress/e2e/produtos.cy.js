fa67de78610b6f39467f8ce1eba46efbdaa8bc3b
lucasosses <lucasosses.qa@gmail.com>
2025-05-26 10:06:13 -0300

    Aula 05 - Módulo 14

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import contrato from '../contratos/produtos.contratos'
 
 describe('Teste de API em Produtos', () => {
 
@@ -6,6 +7,12 @@ describe('Teste de API em Produtos', () => {
     beforeEach(() => {
         cy.token('teste_86038@teste.com', 'teste').then(tkn => {
             token = tkn
+        })
+    });
+
+    it.only('Deve validar contrato deprodutos com sucesso', () => {
+        cy.request('produtos').then(response =>{
+            return contrato.validateAsync(response.body)
         })
     });
 
@@ -41,39 +48,39 @@ describe('Teste de API em Produtos', () => {
             .then(response => {
                 let id = response.body._id
                 cy.request({
-            method: 'PUT',
-            url: `produtos/${id}`,
-            headers: { authorization: token },
-            body: {
-                "nome": produto,
-                "preco": 5499,
-                "descricao": "Notebook Master",
-                "quantidade": 3
-            }
+                    method: 'PUT',
+                    url: `produtos/${id}`,
+                    headers: { authorization: token },
+                    body: {
+                        "nome": produto,
+                        "preco": 5499,
+                        "descricao": "Notebook Master",
+                        "quantidade": 3
+                    }
 
-        }).should((response) => {
-            expect(response.status).equal(200)
-            expect(response.body.message).to.equal('Registro alterado com sucesso')
-        })
+                }).should((response) => {
+                    expect(response.status).equal(200)
+                    expect(response.body.message).to.equal('Registro alterado com sucesso')
+                })
             })
-        
+
     });
 
     it('Deve deletar um produto - DELETE', () => {
         let produto = 'Notebook editado' + Math.floor(Math.random() * 10000000)
         cy.cadastrarProduto(token, produto, 3450, "Notebook editado", 5)
-        .then(response => {
-            let id = response.body._id
-            cy.request({
-            method: 'DELETE',
-            url: `produtos/${id}`,
-            headers: { authorization: token }
+            .then(response => {
+                let id = response.body._id
+                cy.request({
+                    method: 'DELETE',
+                    url: `produtos/${id}`,
+                    headers: { authorization: token }
+                })
+                    .should(resp => {
+                        expect(resp.body.message).to.equal('Registro excluído com sucesso')
+                        expect(resp.status).equal(200)
+                    })
             })
-            .should(resp => {
-                expect(resp.body.message).to.equal('Registro excluído com sucesso')
-                expect(resp.status).equal(200)
-            })
-        })
     })
 
 })
